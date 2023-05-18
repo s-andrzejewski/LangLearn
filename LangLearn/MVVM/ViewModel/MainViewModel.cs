fb507208cab4373b401735ba957339bf872a45b2
Szymon Andrzejewski <s.andrzejewski.7@gmail.com>
2023-05-19 00:19:25 +0200

    Changed Views switching handling concept

--- LangLearn/MVVM/ViewModel/MainViewModel.cs
+++ LangLearn/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using LangLearn.MVVM.View;
+﻿using LangLearn.Core;
+using LangLearn.MVVM.View;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,7 +15,17 @@ namespace LangLearn.MVVM.ViewModel
 {
     internal class MainViewModel : INotifyPropertyChanged
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand QuizViewCommand { get; set; }
+        public RelayCommand AddViewCommand { get; set; }
+        public RelayCommand EditViewCommand { get; set; }
+        public RelayCommand DelViewCommand { get; set; }
+
         public HomeViewModel HomeVM { get; set; }
+        public QuizViewModel QuizVM { get; set; }
+        public AddWordViewModel AddVM { get; set; }
+        public EditWordViewModel EditVM { get; set; }
+        public DeleteWordViewModel DelVM { get; set; }
 
         private object _currentView;
 
@@ -31,7 +42,37 @@ namespace LangLearn.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            QuizVM = new QuizViewModel();
+            AddVM = new AddWordViewModel();
+            EditVM = new EditWordViewModel();
+            DelVM = new DeleteWordViewModel();
+
             CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            QuizViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = QuizVM;
+            });
+
+            AddViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = AddVM;
+            });
+
+            EditViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = EditVM;
+            });
+
+            DelViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = DelVM;
+            });
         }
 
         private RadioButton _selectedOption;
@@ -51,34 +92,6 @@ namespace LangLearn.MVVM.ViewModel
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private void MenuOption_Checked(object sender, RoutedEventArgs e)
-        {
-            RadioButton radioButton = sender as RadioButton;
-            if (radioButton != null)
-            {
-                // Ustawia odpowiedni widok w zależności od zaznaczonej opcji w menu:
-                Debug.WriteLine("name of radio: " + radioButton.Name);
-                switch (radioButton.Name)
-                {
-                    case "QuizOption":
-                        //QuizVM = new QuizViewModel();
-                        //CurrentView = QuizVM;
-                        break;
-                    //case "AddWordOption":
-                     //   CurrentView = AddWordVM;
-                     //   break;
-                    //case "EditWordOption":
-                      //  CurrentView = EditWordVM;
-                      //  break;
-                    //case "DeleteWordOption":
-                      //  CurrentView = DeleteWordVM;
-                     //   break;
-                    default:
-                        break;
-                }
-            }
         }
     }
 }
