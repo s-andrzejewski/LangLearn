296ea5665babc09832250732e8b426c2aa1a7f46
Szymon Andrzejewski <s.andrzejewski.7@gmail.com>
2023-05-18 23:30:55 +0200

    Assigned HomeView on init of the app

--- LangLearn/MVVM/ViewModel/MainViewModel.cs
+++ LangLearn/MVVM/ViewModel/MainViewModel.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using LangLearn.MVVM.View;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace LangLearn.MVVM.ViewModel
 {
     internal class MainViewModel : INotifyPropertyChanged
     {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+
         private RadioButton _selectedOption;
 
         public RadioButton SelectedOption
@@ -25,9 +48,37 @@ namespace LangLearn.MVVM.ViewModel
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void MenuOption_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                // Ustawia odpowiedni widok w zależności od zaznaczonej opcji w menu:
+                Debug.WriteLine("name of radio: " + radioButton.Name);
+                switch (radioButton.Name)
+                {
+                    case "QuizOption":
+                        //QuizVM = new QuizViewModel();
+                        //CurrentView = QuizVM;
+                        break;
+                    //case "AddWordOption":
+                     //   CurrentView = AddWordVM;
+                     //   break;
+                    //case "EditWordOption":
+                      //  CurrentView = EditWordVM;
+                      //  break;
+                    //case "DeleteWordOption":
+                      //  CurrentView = DeleteWordVM;
+                     //   break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
